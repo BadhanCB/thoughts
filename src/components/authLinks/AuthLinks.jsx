5aa427f996acf6b6eb5f2c0b68ff312fb7fa6521
BadhanCB <badhancsesastc112@gmail.com>
2024-01-23 02:01:47 +0600

    designed responsive hamburger menu for mobile device

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -1,19 +1,53 @@
-import Link from 'next/link';
-import styles from './authLinks.module.css';
+"use client";
+import Link from "next/link";
+import styles from "./authLinks.module.css";
+import { FiAlignRight } from "react-icons/fi";
+import { useState } from "react";
 
 const AuthLinks = () => {
-  const status = 'notauthenticated';
+    const [open, setOpen] = useState(false);
+    const status = "authenticated";
 
-  return (
-    <>
-    { status === 'authenticated' ? (<>
-      <Link href='/write'>Write</Link>
-      <button className={styles.logout}>Logout</button>
-    </>) : (
-      <Link href='/login'>Login</Link>
-    )}
-    </>
-  )
-}
+    return (
+        <>
+            {status === "authenticated" ? (
+                <>
+                    <Link className={styles.link} href="/write">
+                        Write
+                    </Link>
+                    <button className={`${styles.logout} ${styles.link}`}>Logout</button>
+                </>
+            ) : (
+                <Link
+                    className={styles.link}
+                    href="/login"
+                >
+                    Login
+                </Link>
+            )}
+            <FiAlignRight
+                onClick={() => setOpen((prev) => !prev)}
+                className={styles.hamburger}
+            />
+            {open && (
+                <div className={styles.mobilemenu}>
+                    <Link href="/">Homepage</Link>
+                    <Link href="/">Contact</Link>
+                    <Link href="/">About</Link>
+                    {status === "authenticated" ? (
+                        <>
+                            <Link href="/write">Write</Link>
+                            <button className={`${styles.logout}`}>
+                                Logout
+                            </button>
+                        </>
+                    ) : (
+                        <Link href="/login">Login</Link>
+                    )}
+                </div>
+            )}
+        </>
+    );
+};
 
 export default AuthLinks;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -16,9 +16,9 @@ const Navbar = () => {
             <div className={styles.logo}>Thoughts</div>
             <div className={styles.links}>
                 <ThemeToggle />
-                <Link href='/'>Homepage</Link>
-                <Link href='/'>Contact</Link>
-                <Link href='/'>About</Link>
+                <Link className={styles.link} href='/'>Homepage</Link>
+                <Link className={styles.link} href='/'>Contact</Link>
+                <Link className={styles.link} href='/'>About</Link>
                 <AuthLinks />
             </div>
         </nav>
