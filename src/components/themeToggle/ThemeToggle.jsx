cd4ad76a46340c76aa9bd716f25be952d1a5cd52
BadhanCB <badhancsesastc112@gmail.com>
2024-01-22 19:59:44 +0600

    designed theme toggler

--- src/components/themeToggle/ThemeToggle.jsx
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -1,8 +1,14 @@
+import { IoMoon, IoSunny } from "react-icons/io5";
+import { FaCircle } from "react-icons/fa6";
 import styles from './themeToggle.module.css';
 
 const ThemeToggle = () => {
   return (
-    <div>ThemeToggle</div>
+    <div className={styles.container}>
+      <IoSunny />
+      <FaCircle className={styles.themeTogglerCircle} />
+      <IoMoon />
+    </div>
   )
 }
 
